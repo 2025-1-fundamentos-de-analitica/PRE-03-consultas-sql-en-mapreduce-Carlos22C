4c1e210a7bd48593da1f7086e252dcbdb0c48c5e
Carlos22C <ccardonamo@unal.edu.co>
2025-04-30 10:12:55 -0500

    Actividad semana 4

---
+++ homework/__init__.py
@@ -0,0 +1,29 @@
+"""Taller evaluable"""
+
+# pylint: disable=broad-exception-raised
+# pylint: disable=import-error
+
+from .mapreduce  import run_mapreduce_job  # type: ignore
+
+
+#
+# Columns:
+# total_bill, tip, sex, smoker, day, time, size
+#
+
+#
+# SELECT *, tip/total_bill as tip_rate
+# FROM tips;
+#
+
+
+#
+# ORQUESTADOR:
+#
+def run():
+    """Orquestador"""
+
+
+if __name__ == "__main__":
+
+    run()
